6bbbd39240c3395b8db06642510d002d76c8216c
Travis Molter <travismolter@gmail.com>
2019-12-02 14:50:07 -0800

    getCaster will grab castToString

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -97,6 +97,7 @@ describe('validator module', () => {
   
   it('can get the right caster', () => {
     expect(getCaster(Number)).toEqual(castToNumber);
+    expect(getCaster(String)).toEqual(castToString);
     expect(getCaster(Promise)).toBeNull();
   });
 });
