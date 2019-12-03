a95aeccc56012057cf4e16ab8b81e5c36783066e
Travis Molter <travismolter@gmail.com>
2019-12-02 16:03:34 -0800

    lab should be complete

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require
 
 console.log(isNumber('3'));
 console.log(isNumber(3));
-// console.log(isString('hey'));
-// console.log(isBoolean('hey'));
-// console.log(isArray('hey'));
-// console.log(isObject('hey'));
-// console.log(isFunction('hey'));
+console.log(isString(2));
+console.log(isString('hey'));
+console.log(isBoolean('hey'));
+console.log(isBoolean(false));
+console.log(isArray('hey'));
+console.log(isArray(['hey']));
+console.log(isObject('hey'));
+console.log(isObject({ name: 'travis ' }));
+console.log(isFunction('hey'));
+console.log(isFunction(() => {}));
